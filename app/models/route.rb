c1ca8b6c9c9c97dd9fdbbf1b4204c22512ad3abe
jenyau <jekharitonov@gmail.com>
2020-05-16 20:22:28 +0300

    Revert "Revert "complete studying lesson17""
    This reverts commit d22d528ec73d2c2eae700254492b42f77af65ce0.

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,4 +1,20 @@
 class Route < ApplicationRecord
   has_many :railway_stations_routes
   has_many :railway_stations, through: :railway_stations_routes
+  validates :name, presence: true
+  before_validation :set_name
+  validate :stations_count
+  #default_scope { order("created_at DESC") 
+
+  private
+  def set_name
+    self.name = "#{railway_stations.first.title} - #{railway_stations.last.title}"
+  end
+
+  def stations_count
+    if railway_stations.size < 2
+      errors.add(:base, 'Route should be contain at least 2 stations')
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
-  resources :railway_stations
-  resources :trains
-  resources :routes
+  devise_for :users
+
+  resources :tickets, only: [:show, :create]
+  resource :search, only: [:new, :edit, :show]
+
+  namespace :admin do
+    resources :railway_stations do
+      patch :update_position, on: :member
+    end
+    resources :trains do
+      resources :carriages, shallow: true
+    end
+    resources :routes
+  end
 
   get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
